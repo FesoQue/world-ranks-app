249f4e6be2ecc1c7ed86fbcf4b874d65ff14deb1
FesoQue <adefesoq@gmail.com>
2022-04-04 02:26:00 +0100

    world rank app

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css';
+
+const HomePage = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>main</main>
+      <footer className={styles.footer}>footer</footer>
+    </div>
+  );
+};
+
+export default HomePage;
