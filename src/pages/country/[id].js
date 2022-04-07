36c03a43560a13e48a546d6194e1502cc12ae0e0
FesoQue <adefesoq@gmail.com>
2022-04-07 17:03:50 +0100

    country page wip

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,22 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../../component/Layout/Layout';
 import styles from './country.module.css';
 
+const getCountry = async (id) => {
+  const res = await fetch(`https://restcountries.com/v2/alpha/${id}`);
+  const country = await res.json();
+  return country;
+};
+
 const Country = ({ country }) => {
+  const [border, setBorder] = useState([]);
+
+  const getBorders = async () => {
+    const borders = await Promise.all(
+      country.borders.map((border) => getCountry(border))
+    );
+    setBorder(borders);
+  };
+
+  useEffect(() => {
+    getBorders();
+  }, []);
+
   return (
     <Layout title={country.name}>
       <div>
         <div className={styles.overview_panel}>
           <img src={country.flag} alt={country.name} />
-          <h1>{country.name}</h1>
-          <div>{country.region}</div>
+          <h1 className={styles.overview_name}>{country.name}</h1>
+          <div className={styles.overview_region}>{country.region}</div>
 
-          <div>
+          <div className={styles.overview_numbers}>
             <div className={styles.overview_population}>
-              <div>{country.population}</div>
-              <div>Population</div>
+              <div className={styles.overview_value}>{country.population}</div>
+              <div className={styles.overview_label}>Population</div>
             </div>
-            <div className={styles.overview_area}></div>
+            <div className={styles.overview_area}>
+              <div className={styles.overview_value}>{country.area}</div>
+              <div className={styles.overview_label}>Area</div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.details_panel}>
+          <h1 className={styles.details_panel_heading}>Details</h1>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Capital</div>
+            <div className={styles.details_panel_value}>{country.capital}</div>
+          </div>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Subregion</div>
+            <div className={styles.details_panel_value}>
+              {country.subregion}
+            </div>
+          </div>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Languages</div>
+            <div className={styles.details_panel_value}>
+              {country.languages.map((countryname) => {
+                return countryname.name;
+              })}
+            </div>
+          </div>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Currencies</div>
+            <div className={styles.details_panel_value}>
+              {country.currencies.map((countryname) => {
+                return countryname.name;
+              })}
+            </div>
+          </div>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Native name</div>
+            <div className={styles.details_panel_value}>{country.name}</div>
+          </div>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Gini</div>
+            <div className={styles.details_panel_value}>{country?.gini}%</div>
+          </div>
+
+          <div className={styles.details_panel_borders}>
+            {border.map(({ flag, name, alpha3Code }) => {
+              return (
+                <div key={alpha3Code}>
+                  <img src={flag} alt={name} />
+                  <div>{name}</div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -27,10 +104,8 @@ const Country = ({ country }) => {
 export default Country;
 
 export const getServerSideProps = async ({ params }) => {
-  const res = await fetch(`https://restcountries.com/v2/alpha/${params.id}`);
-  const country = await res.json();
+  const country = await getCountry(params.id);
 
-  console.log(params.id);
   return {
     props: {
       country,
